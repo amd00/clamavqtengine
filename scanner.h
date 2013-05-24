c2cd5452c11e5661f91c7486a6c2a4a229a8bada
Andrey Dudakov <amd00@amd00-desktop.orion.org>
2013-05-24 11:42:28 +0400

    GPL v2 added

--- scanner.h
+++ scanner.h
@@ -1,4 +1,25 @@
 
+/*
+ *  Copyright (C) 2013 Andrey Dudakov
+ *
+ *  Authors: Andrey "amd00" Dudakov
+ *
+ *  This file is part of clamavqtengine.
+ *
+ *  clamavqtengine is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  clamavqtengine is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with clamavqtengine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _SCANNER_H_
 #define _SCANNER_H_
 
